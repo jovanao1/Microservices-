d62724828cc91ea69359206b929bcf19e71b2de7
jovanao1 <jocaa.obrenic@gmail.com>
2026-08-26 15:55:01 +0200

    simple change

---
+++ racun-service/src/main/java/com/example/racunservice/RacunServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.racunservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacunServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacunServiceApplication.class, args);
+    }
+
+}
