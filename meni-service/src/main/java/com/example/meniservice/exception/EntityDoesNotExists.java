47a87ea4c9b965df8686f725edd1c8d240858fb1
jovanao1 <jocaa.obrenic@gmail.com>
2026-08-26 15:50:14 +0200

    simple change

---
+++ meni-service/src/main/java/com/example/meniservice/exception/EntityDoesNotExists.java
@@ -0,0 +1,10 @@
+package com.example.meniservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EntityDoesNotExists extends RuntimeException {
+    private String message;
+}
