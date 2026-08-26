ef345d437aff38c2501db5309dfef28a9b568404
jovanao1 <jocaa.obrenic@gmail.com>
2026-08-26 15:59:00 +0200

    simple change

---
+++ racun-service/src/main/java/com/example/racunservice/repositories/RezervacijaracunRepository.java
@@ -0,0 +1,7 @@
+package com.example.racunservice.repositories;
+
+import com.example.racunservice.model.Rezervacijaracun;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RezervacijaracunRepository extends JpaRepository<Rezervacijaracun, Integer> {
+}
