b9ea452d5e9c40f4b8da8a5df9e9c76051d939c8
jovanao1 <jocaa.obrenic@gmail.com>
2026-08-26 15:58:27 +0200

    simple change

---
+++ racun-service/src/main/java/com/example/racunservice/repositories/NarudzbinaracunRepository.java
@@ -0,0 +1,7 @@
+package com.example.racunservice.repositories;
+
+import com.example.racunservice.model.Narudzbinaracun;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NarudzbinaracunRepository extends JpaRepository<Narudzbinaracun, Integer> {
+}
