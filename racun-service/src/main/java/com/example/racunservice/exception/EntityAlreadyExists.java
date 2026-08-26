c2ceb26fa64777451fe65d83ff45c60a8e9ed7c9
jovanao1 <jocaa.obrenic@gmail.com>
2026-08-26 16:01:22 +0200

    simple change

---
+++ racun-service/src/main/java/com/example/racunservice/exception/EntityAlreadyExists.java
@@ -0,0 +1,10 @@
+package com.example.racunservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EntityAlreadyExists extends RuntimeException{
+    private String message;
+}
