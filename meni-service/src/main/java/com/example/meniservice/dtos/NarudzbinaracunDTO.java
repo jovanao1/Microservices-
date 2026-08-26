e766496f5a78ae117354156bb005e9c66ad989fd
jovanao1 <jocaa.obrenic@gmail.com>
2026-08-26 15:51:57 +0200

    simple change

---
+++ meni-service/src/main/java/com/example/meniservice/dtos/NarudzbinaracunDTO.java
@@ -0,0 +1,12 @@
+package com.example.meniservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NarudzbinaracunDTO {
+    private Integer id;
+    private Double ukupnaCenaNarudzbina;
+
+}
