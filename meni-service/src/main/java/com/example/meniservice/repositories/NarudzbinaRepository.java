06ec087b79a58d21d3502dc563ec67acd79f3674
jovanao1 <jocaa.obrenic@gmail.com>
2026-08-26 15:46:57 +0200

    simple change

---
+++ meni-service/src/main/java/com/example/meniservice/repositories/NarudzbinaRepository.java
@@ -0,0 +1,6 @@
+package com.example.meniservice.repositories;
+import com.example.meniservice.model.Narudzbina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NarudzbinaRepository extends JpaRepository<Narudzbina, Integer> {
+}
